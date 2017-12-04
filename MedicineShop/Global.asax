163291b0af9d1d4f808598ebaf7c00265d4a0c22
noyon <noyon@DESKTOP-HBF8BPB>
2017-12-05 04:27:49 +0600

    Add project files.

---
+++ MedicineShop/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MedicineShop.MvcApplication" Language="C#" %>
---
+++ MedicineShop/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MedicineShop.Startup))]
+namespace MedicineShop
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
